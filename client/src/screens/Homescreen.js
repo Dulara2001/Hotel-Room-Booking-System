44aa121e55171e841fc96350873cf0574f41d9d5
dulara <it21014840@my.sliit.lk>
2023-01-10 19:20:51 +0530

    8th commit

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,27 +1,47 @@
-import React , {useState , useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios";
+import Room from "../components/Room";
 
-function Homescreen(){
+function Homescreen() {
 
-    const[rooms , setrooms] = useState([])
+    const [rooms, setrooms] = useState([])
+    const [loading, setloding] = useState()
+    const [error, seterror] = useState()
 
     useEffect(() => {
-        async function fetchData() { 
-          try {
-            const data = (await axios.get('/api/rooms/getallrooms')).data;
-            setrooms(data);   
-          } catch (error) { 
-            console.error(error);  
-          }  
-        }
-     fetchData(); 
-      }, []);
+        async function fetchData() {
+            try {
+                setloding(true)
+                const data = (await axios.get('/api/rooms/getallrooms')).data
 
-    
-    return(
-        <div>
-            <h1>home screen</h1>
-            <h1>there are {rooms.length} rooms</h1>
+                setrooms(data)
+                setloding(false)
+
+            } catch (error) {
+                seterror(true)
+                console.error(error);
+                setloding(false)
+            }
+        }
+        fetchData();
+    }, []);
+
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center mt-5">
+                {loading ? (
+                  <h1>loading...</h1>
+                ) : error ? (
+                  <h1>Error</h1>
+                ) : (
+                  rooms.map((room) => {
+                    return <div className="col-md-9 mt-2">
+                        <Room room={room}/>
+                    </div>
+
+                }))}
+            </div>
         </div>
     )
 }
