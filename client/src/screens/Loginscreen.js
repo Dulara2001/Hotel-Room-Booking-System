d441ae37646abbe2241c8a6b8cf04921186ddea4
dulara <it21014840@my.sliit.lk>
2023-01-13 09:40:16 +0530

    14th commit

--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react'
+import axios from "axios";
 
 function Loginscreen() {
 
 
     const [email, setemail] = useState('')
     const [password, setpassword] = useState('')
-    
 
-    function Login() {
-        
-        
-            const user={
-              
-                email,
-                password,
-               
-            }
-            console.log(user)
-        
+
+    async function Login() {
+
+
+        const user = {
+
+            email,
+            password,
+
+        }
+        try {
+            const result = await axios.post('/api/users/login', user).data
+            
+        }catch(error){
+            console.log(error)
+        }
+        console.log(user)
+
     }
 
 
@@ -28,13 +35,13 @@ function Loginscreen() {
 
                     <div className='bs'>
                         <h2>Login</h2>
-                        
+
                         <input type="text" className="form-control" placeholder="email"
                             value={email} onChange={(e) => { setemail(e.target.value) }} />
 
                         <input type="text" className="form-control" placeholder="password"
                             value={password} onChange={(e) => { setpassword(e.target.value) }} />
-                       
+
 
                         <button className='btn btn-primary mt-3' onClick={Login}>Login</button>
                     </div>
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 
 const dbConfig = require('./db')
 const roomsRoute = require('./routes/roomsRoute')
+const usersRoute = require('./routes/usersRoute')
 
 
 app.use(express.json())
 
 app.use('/api/rooms', roomsRoute)
+app.use('/api/users', usersRoute)
 
 
 const PORT = process.env.PORT || 5000;
