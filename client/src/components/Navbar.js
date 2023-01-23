cbceb6bbfca7bcb366dd35932babe098de9730f2
dulara <it21014840@my.sliit.lk>
2023-01-23 19:19:08 +0530

    16th commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function Navbar() {
+
     // const user = JSON.parse(localStorage.getItem('currentUser'));
+
     return (
         <div>
             <nav class="navbar navbar-expand-lg">
@@ -11,14 +13,25 @@ function Navbar() {
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
+                        <ul class="navbar-nav mr-5">
+                            {/* {user ? <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    {user.name}
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Bookings</a>
+    <a class="dropdown-item" href="#">Logout</a>
+ 
+  </div>
+</div>(<><h1 style={{ color: 'white' }}>{user.name}</h1></>) : (<> */}
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="/Register">Register</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="/login">Login</a>
                             </li>
-                            
+                            {/* </>)} */}
+
                         </ul>
                     </div>
                 </div>
